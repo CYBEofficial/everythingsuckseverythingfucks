5716d4c1627e66e34fcab76b1142f5b676f7c286
jmefdev <94146423+jmefdev@users.noreply.github.com>
2025-08-10 17:53:17 -0600

    Fix frameborder console error and improve iframe formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -244,7 +244,7 @@ export default function Home() {
              scrolling="no" 
              frameBorder="no" 
              allow="autoplay" 
-             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2138170161%3Fsecret_token%3Ds-MHqVhA2z3Wz&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2138170161%3Fsecret_token%3Ds-MHqVhA2z3Wz&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
            ></iframe>
            <div style={{
              fontSize: '10px', 
@@ -257,7 +257,7 @@ export default function Home() {
              fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',
              fontWeight: 100
            }}>
-             <a href="https://soundcloud.com/billain" title="Billain" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Billain</a> · <a href="https://soundcloud.com/billain/esef/s-MHqVhA2z3Wz" title="ESEF" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>ESEF</a>
+             <a href="https://soundcloud.com/billain" title="Billain" target="_blank" rel="noopener noreferrer" style={{color: '#cccccc', textDecoration: 'none'}}>Billain</a> · <a href="https://soundcloud.com/billain/esef/s-MHqVhA2z3Wz" title="ESEF" target="_blank" rel="noopener noreferrer" style={{color: '#cccccc', textDecoration: 'none'}}>ESEF</a>
            </div>
           </div>
 
